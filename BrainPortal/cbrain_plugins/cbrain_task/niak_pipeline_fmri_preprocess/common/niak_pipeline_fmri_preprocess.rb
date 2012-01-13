58a63ebd41df0783327477de8bbfdac68834afed
Pierre Rioux <pierre.rioux@mcgill.ca>
2012-01-12 21:02:20 -0500

    New user show page. SeaMonkey detection.
    Former-commit-id: 90a7ae03538eb326b1719fd712b1f3467bf22128

---
+++ BrainPortal/cbrain_plugins/cbrain_task/niak_pipeline_fmri_preprocess/common/niak_pipeline_fmri_preprocess.rb
@@ -0,0 +1,14 @@
+
+#
+# CBRAIN Project
+#
+# Model NiakPipelineFmriPreprocess
+#
+
+class CbrainTask::NiakPipelineFmriPreprocess
+
+  def self.pretty_type
+    "NIAK fMRI Preprocessing Pipeline"
+  end
+
+end
